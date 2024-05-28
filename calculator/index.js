c37d8f955d1e315563f83d1c78eb665453acdee5
Korozya-bit <shaigimel@gmail.com>
2024-05-28 23:37:54 +0300

    create cps test & calculator

---
+++ calculator/index.js
@@ -0,0 +1,30 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(toAppend){
+    display.value += toAppend;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Error!"
+        console.error("Sytax Error!")
+    }
+}
+
+function isOperation(key){
+    return key == '-' || key == '+' || key == '*' || key == '/' || key == '=' || key == '.';
+}
+
+document.addEventListener('keydown', function(event) {
+    if (event.key == 'c') clearDisplay();
+    if (event.key == 'Enter') calculate();
+    if (event.key >= '0' && event.key <= '9') display.value += event.key;
+    if (event.key == 'Backspace') display.value = String(display.value).substring(0, String(display.value).length - 1);
+    if (isOperation(event.key)) display.value += event.key;
+})
