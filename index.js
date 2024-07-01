9f9c058da894159e307af9e7715dbb83fe0d9210
TapChap <shaigimel@gmail.com>
2024-07-01 16:53:40 +0300

    minor changes

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ io.on('connection', (socket) => {
     })
   });  
 
-server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+server.listen(2407, () => {
+  console.log('server live.');
 });
 
 function popRandomColor(){
